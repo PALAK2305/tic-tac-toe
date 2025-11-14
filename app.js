769ad3efc97eb53819c34ada24a4d296f5181513
Palak Damgude <damgudepalak@gmail.com>
2025-11-14 23:09:16 +0530

    app.js

---
+++ app.js
@@ -0,0 +1,60 @@
+let boxes = document.querySelectorAll(".box");
+let reset = document.querySelector("#reset");
+let btnstart = document.querySelector("#btnstart");
+let msgcon = document.querySelector(".msgContent");
+let msg = document.querySelector("#msg");
+
+let turno = true; //playerx,playero
+
+const winPatterns = [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8]
+];
+
+const resetgame = () => {
+    turno = true;
+}
+
+boxes.forEach((box) => {
+    box.addEventListener("click", ()=> {
+        console.log("box was clicked");
+        if(turno){
+            box.innerText = "O";
+            turno = false;
+        } else {
+            box.innerText="X";
+              turno = true;
+        }
+        box.disabled = true;
+
+        checkWinner();
+    });
+});
+
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations, Winner is ${winner}`;
+    msgcon.classList.remove("hide");
+}
+
+
+const checkWinner = () => {
+    for (let pattern of winPatterns) {
+         let postVal1 = boxes[pattern[0]].innerText;
+         let postVal2 = boxes[pattern[1]].innerText;
+         let postVal3 = boxes[pattern[2]].innerText;
+        
+        if(postVal1!= "" && postVal2 != "" && postVal3 != "") {
+            if (postVal1 === postVal2 && postVal2 === postVal3) {
+                console.log("winner", postVal1);
+                showWinner(postVal1);
+            }
+        }
+    }
+};
+
